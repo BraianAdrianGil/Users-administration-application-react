bdccfd90a8d238d6507970ed822301a44a466554
BraianAdrian <braian.adrian.gagliardo@gmail.com>
2023-06-30 19:47:05 -0300

    tst

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://users-crud.academlo.tech/",
+  baseURL: "https://users-crud-node-j6c6.onrender.com",
 });
